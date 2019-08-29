40c05baa6354ed21b2324ed8bf4e6f545e5abfa7
Dominique BELLO <dominique.bello@outlook.fr>
2019-08-29 14:31:38 +0200

    First commit

---
+++ build/precache-manifest.e4ee54027a843f2a3f4c84ee91167d5b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4f410ab03ce69c2fa0d49fdfe0d01d13",
+    "url": "/UDEMY_LightsOut_app/index.html"
+  },
+  {
+    "revision": "f6353ccdd57977f43ccc",
+    "url": "/UDEMY_LightsOut_app/static/css/main.9ec2d78d.chunk.css"
+  },
+  {
+    "revision": "5dde642eb0f3995c2c6b",
+    "url": "/UDEMY_LightsOut_app/static/js/2.e00c9271.chunk.js"
+  },
+  {
+    "revision": "f6353ccdd57977f43ccc",
+    "url": "/UDEMY_LightsOut_app/static/js/main.d9a81311.chunk.js"
+  },
+  {
+    "revision": "06deed5307c257f0a364",
+    "url": "/UDEMY_LightsOut_app/static/js/runtime~main.c35b8331.js"
+  }
+]);
